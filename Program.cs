0991bb04115b173f2497697c6f3222f1e3ebe76c
Jordan Leich <jordanleich@gmail.com>
2020-06-05 12:09:21 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Made by Jordan Leich on 6/5/2020
+
+using System;
+using System.Threading;
+
+namespace Infinite_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the infinite adding loop! ");
+            Console.WriteLine("");
+            Thread.Sleep(3000);
+
+            int Number = 0;
+            int Another_Number = 0;
+
+            while (Number == 0)
+            {
+                Another_Number += 1;
+                Console.WriteLine(Another_Number);
+                Thread.Sleep(75);
+            }
+        }
+    }
+}
